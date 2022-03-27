7584837e64feae3146b12b72eb9b783a353e8bba
santanam23 <mirmir11.ms@gmail.com>
2022-03-27 10:39:37 -0500

    created user, thought. reaction routes

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
-const reactionRoutes = require('./reaction-routes');
 
 
 router.use('/user', userRoutes);
 router.use('thought', thoughtRoutes);
-router.use('/reaction', reactionRoutes);
 
 module.exports = router;
---
+++ controllers/user-controller.js
@@ -0,0 +1,8 @@
+const { User } = require('../models');
+
+const userController = {
+  // the functions will go in here as methods
+  
+};
+
+module.exports = pizzaController;
